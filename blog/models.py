ab8c9b229204ae10e6ab767ef228ce43f0870d30
DzygunRoman <dzygun-roman@mail.ru>
2024-01-13 14:17:16 +0300

    задания порядка отображения постов - класс Meta

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    body = models.TextField()
+    title = models.CharField(max_length=250) #заголовок
+    slug = models.SlugField(max_length=250) #слаг
+    body = models.TextField() #тело поста
+    publish = models.DateTimeField(default=timezone.now) #время публикации
+    created = models.DateTimeField(auto_now_add=True) #время создания
+    updated = models.DateTimeField(auto_now=True) #время изменения
+
+    class Meta:
+        ordering = ['-publish']
+
 
     def __str__(self):
         return self.title
