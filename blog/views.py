45ceac19e8e750ae9cea0ed48a35cc7ba79cd99c
DzygunRoman <dzygun-roman@mail.ru>
2024-01-16 14:41:50 +0300

    создание шаблона отображения поста

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
+
 def post_list(request): #представление извлечь все посты со статусом PUBLISHED
     posts = Post.published.all()
-    return render(request,'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, id):#извлекаем объект с заданным id, вызвав метод get() стандартного менеджера objects
