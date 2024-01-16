1ab536aac5e5168e5a1cbbed91450c2e596b85f5
DzygunRoman <dzygun-roman@mail.ru>
2024-01-16 10:53:21 +0300

    настройка админ-панели

--- blog/admin.py
+++ blog/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-@admin.register(Post)
+@admin.register(Post) #добавляем в админ-панель класс модели Post
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-    list_filter = ['status', 'created', 'publish', 'author']
-    search_fields = ['title', 'body']
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
-    date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
+    list_display = ['title', 'slug', 'author', 'publish', 'status']#поля отображаемые в админ-панели
+    list_filter = ['status', 'created', 'publish', 'author']#правая боковая панель для фильтрации
+    search_fields = ['title', 'body'] #создание строки поиска
+    prepopulated_fields = {'slug': ('title',)}#автозаполнение поля слаг
+    raw_id_fields = ['author'] #создание поискового виджета по полю автор
+    date_hierarchy = 'publish' #навигация по иерархии дат
+    ordering = ['status', 'publish'] #сортировка по умолчанию по указанным полям
