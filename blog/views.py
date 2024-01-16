0b6d70e57dced0427fb45169fc06c3370c8daa23
DzygunRoman <dzygun-roman@mail.ru>
2024-01-16 13:30:44 +0300

    создание представлений и маршрутов

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_list(request): #представление извлечь все посты со статусом PUBLISHED
+    posts = Post.published.all()
+    return render(request,'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):#извлекаем объект с заданным id, вызвав метод get() стандартного менеджера objects
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'blog/post/detail.html', {'post': post})#прорисовка извлеченного поста с использованием шаблона
+
+
+
