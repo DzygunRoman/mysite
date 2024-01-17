5e6eeeb2a76a41df810ee1cc775b92b7884a017c
DzygunRoman <dzygun-roman@mail.ru>
2024-01-17 09:47:16 +0300

    изменение post_detail для извлечения поста по слагу на дату публикации

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,13 @@ def post_list(request): #представление извлечь все пос
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):#извлекаем объект с заданным id, вызвав метод get() стандартного менеджера objects
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):#извлекаем пост с заданным cлагом и датой публикации
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish_year=year,
+                             publish_month=month,
+                             publish_day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})#прорисовка извлеченного поста с использованием шаблона
 
