d7e03c21bd4fc57ea044d2037271bd5c98af5593
DzygunRoman <dzygun-roman@mail.ru>
2024-01-19 11:21:13 +0300

    добавляю данные из модели Comments в админ-панель

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comments
 
 
 @admin.register(Post) #добавляем в админ-панель класс модели Post
@@ -11,3 +11,10 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author'] #создание поискового виджета по полю автор
     date_hierarchy = 'publish' #навигация по иерархии дат
     ordering = ['status', 'publish'] #сортировка по умолчанию по указанным полям
+
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
