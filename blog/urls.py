e0f01a43db70ec8a28312e70f03b31dabdf075e1
DzygunRoman <dzygun-roman@mail.ru>
2024-01-22 15:18:22 +0300

    создание класса новостной ленты

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'  # определяем именное пространство приложения приложения
 
@@ -13,5 +14,5 @@ urlpatterns = [
          name='post_detail'),  # <> - для захвата значений из url-адреса
     path('<int:post_id>/share/', views.post_share, name='post_share'),#шаблон почты
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
