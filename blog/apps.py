1b8940f6f0385b0539f77a56ad6c1ff92576cb73
DzygunRoman <dzygun-roman@mail.ru>
2024-01-22 09:39:10 +0300

    подключаем файл шаблона формы comment_form.html

--- blog/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    verbose_name = "Известные женщины мира" #название приложения в админ-панели
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
