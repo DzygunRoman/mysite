7900b39d9fe2be913db0bf6d985a53f810054efd
Roman Dzygun <dzygun-roman@mail.ru>
2024-06-15 08:31:02 +0300

    admin.py задаю поля модели для отображения в админ-панели

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)  # регистрирую модель Post в админ-панели
-# Register your models here.
+admin.register(Post)  # регистрирую модель Post в админ-панели
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']  #задаем поля модели для отображения на админ-панели
