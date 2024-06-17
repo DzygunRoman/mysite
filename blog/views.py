1d26fac20d4da2c0e0ffd42d717772496dffbf31
DzygunRoman <dzygun-roman@mail.ru>
2024-06-17 09:11:00 +0300

    поправил представление о выводе одного поста с помощью функции get_object_or_404, которая выводит пост либо исключение если поста не существует

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
@@ -8,9 +8,6 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})  # прорисовка шаблона
 
 
-def post_detail(request, id): # извлекаю пост по id и в render отправляю в шаблон
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Пост не найден.")
+def post_detail(request, id):  # извлекаю пост по id и в render отправляю в шаблон
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)  # функция либо извлекает пост либо выдает исключение
     return render(request, 'blog/post/detail.html', {'post': post})
