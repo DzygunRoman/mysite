ad6e0d09cd3b1b6e63717c14974f6b573bf0191b
DzygunRoman <dzygun-roman@mail.ru>
2024-06-17 08:56:54 +0300

    создание представления к модели post для вывода всех постов на разметку

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all() # извлекаются все посты при помощи ранее созданного менеджера
+    return render(request, 'blog/post/list.html', {'posts': posts})  # прорисовка шаблона
+
