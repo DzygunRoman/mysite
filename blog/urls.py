96be25622cbb387ad3967074a2bbe8cd9ae4923c
DzygunRoman <dzygun-roman@mail.ru>
2024-06-18 12:25:21 +0300

    добавил url post_comment

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'), # путь для всех посто на основе классов
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),  # путь для одного поста
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
