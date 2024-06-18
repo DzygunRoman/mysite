20c22b654d9c84067d334822632f176f6adc5a9f
DzygunRoman <dzygun-roman@mail.ru>
2024-06-18 12:07:58 +0300

    добавил модель Comment в админ-панель

--- blog/models.py
+++ blog/models.py
@@ -49,19 +49,19 @@ class Post(models.Model):
                              self.publish.day,
                              self.slug])  # динамически сформированный адрес
 
-    class Comment(models.Model):
-        post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments' )
-        name = models.CharField(max_length=80)
-        email = models.EmailField()
-        body = models.TextField()
-        created = models.DateTimeField(auto_now_add=True)
-        updated = models.DateTimeField(auto_now=True)
-        active = models.BooleanField(default=True)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments' )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
-        class Meta:
-            ordering = ['created']
-            indexes = [
-                models.Index(fields=['crated']),
-            ]
-        def __str__(self):
-            return f"Комментарий {self.name} на {self.post}"
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return f"Комментарий {self.name} на {self.post}"
