3f8ecd24f9a034d317f573f6f92ab35822240e07
DzygunRoman <dzygun-roman@mail.ru>
2024-06-18 12:13:22 +0300

    создание формы из модели в файле forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25) # имя человека отправляющего пост
-    email = forms.EmailField() # почта отправившего пост
-    to = forms.EmailField() # почта получателя поста
-    comments = forms.CharField(required=False, widget=forms.Textarea)  #  комментарий отправленный сообщением по электронной почте
-
+    name = forms.CharField(max_length=25)  # имя человека отправляющего пост
+    email = forms.EmailField()  # почта отправившего пост
+    to = forms.EmailField()  # почта получателя поста
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)  # комментарий отправленный сообщением по электронной почте
