c6aa0b85914a2c77b48dbac2a19093c4400880b2
DzygunRoman <dzygun-roman@mail.ru>
2024-06-18 09:23:53 +0300

    обработка ошибок при пагинации ввода списка постов в представлении post-list

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 
 
 def post_list(request):
     post_list = Post.published.all()  # извлекаются все посты при помощи ранее созданного менеджера
-    paginator = Paginator(post_list, 3) # постраничная разбивка
+    paginator = Paginator(post_list, 3)  # постраничная разбивка
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)  # если номер страницы не int то выдавать первую страницу
+    except EmptyPage:
+        posts = paginator.page(
+            paginator.num_pages)  # если номер страницы находится в не диапазона то показать последнюю
     return render(request, 'blog/post/list.html', {'posts': posts})  # прорисовка шаблона
 
 
